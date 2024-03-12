66fa3556e97e00ae0586d65f044ff2f0f3f09677
AbhijitKr <abhijitkr600@gmail.com>
2024-03-12 14:00:21 +0530

    Adds the filter section with Overview title

--- src/payments/Payouts.jsx
+++ src/payments/Payouts.jsx
@@ -9,6 +9,17 @@ export default function Payouts() {
       </div>
       <div className="w-[83%]">
         <Header />
+        <div className="flex justify-between py-5 px-10">
+          <h3 className="text-xl font-medium">Overview</h3>
+          <div className="text-gray-600 border p-1 px-1 pr-2">
+            <select name="filterMonth" id="" className="px-1 outline-none">
+              <option value="lastMonth" selected>
+                Last Month
+              </option>
+              <option value="jan">January</option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   );
